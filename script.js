4b60fd99cb69e8119b77a7f76d099eb76b83f1b0
Gundulin <47258470+Gundulin@users.noreply.github.com>
2019-05-15 20:47:44 -0600

    Penultimate Update

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-window.onload = async function() {
-    const url = "https://www.boredapi.com/api/activity?type=recreational?participants=1";
+/** https://www.boredapi.com/ */
 
-    var numParticipants;
+/** Returns a suggestion for a random activity */
+async function randSuggest(type) {
+    console.log("IN RAND SUGGEST");
+    const url = "https://www.boredapi.com/api/activity" + type;
+    changeButtonColor(type);
 
     var response = await fetch(url);
     console.log("response= ", response);
     const json = await response.json();
     console.log(json);
 
-    
-
-    // By myself
-    // With friends
-
-    // numPeople?
-    // free? (check accessibility)
-    // type "relaxation"
-
-    // ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+    let output = "";
+    output += json.activity;
+  
+    document.getElementById("theSuggestion").innerHTML= output;
 }
 
-/**
- * Button for each type
- * 
- * RANDOM?
- * 
- * 
- * 
- * number people?
- * What kind? ["education, etc"]
- * "How loaded are you?"
- * 
- * 
- * 
- */
+function changeButtonColor(name) {
+    resetButtons();
+    document.getElementById(name).style.backgroundColor= "green";
+}
 
+function resetButtons() {
+    document.getElementById("/").style.backgroundColor= "navy";
+    document.getElementById("?type=recreational").style.backgroundColor= "navy";
+    document.getElementById("?type=education").style.backgroundColor= "navy";
+    document.getElementById("?type=social").style.backgroundColor= "navy";
+    document.getElementById("?type=diy").style.backgroundColor= "navy";
+    document.getElementById("?type=charity").style.backgroundColor= "navy";
+    document.getElementById("?type=cooking").style.backgroundColor= "navy";
+    document.getElementById("?type=music").style.backgroundColor= "navy";
+    document.getElementById("?type=busywork").style.backgroundColor= "navy";
+    document.getElementById("?type=relaxation").style.backgroundColor= "navy";
+    document.getElementById("?participants=1").style.backgroundColor= "navy";
+    document.getElementById("?participants=2").style.backgroundColor= "navy";
+    document.getElementById("?participants=3").style.backgroundColor= "navy";
+    document.getElementById("?participants=4").style.backgroundColor= "navy";
+    document.getElementById("?participants=5").style.backgroundColor= "navy";
+
+    // document.getElementById("/").style.hover.backgroundColor= "rgba(0, 0, 128, 0.5)";
+    // document.getElementById("?type=recreational").style.backgroundColor= "navy";
+    // document.getElementById("?type=education").style.backgroundColor= "navy";
+    // document.getElementById("?type=social").style.backgroundColor= "navy";
+    // document.getElementById("?type=diy").style.backgroundColor= "navy";
+    // document.getElementById("?type=charity").style.backgroundColor= "navy";
+    // document.getElementById("?type=cooking").style.backgroundColor= "navy";
+    // document.getElementById("?type=music").style.backgroundColor= "navy";
+    // document.getElementById("?type=busywork").style.backgroundColor= "navy";
+    // document.getElementById("?participants=1").style.backgroundColor= "navy";
+    // document.getElementById("?participants=2").style.backgroundColor= "navy";
+    // document.getElementById("?participants=3").style.backgroundColor= "navy";
+    // document.getElementById("?participants=4").style.backgroundColor= "navy";
+    // document.getElementById("?participants=5").style.backgroundColor= "navy";
+    
+}
